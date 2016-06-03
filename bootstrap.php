5bcb01a4f9e90ee9711f988912aeded0ceb2eb34
Jake Toolson <jaketoolson@gmail.com>
2016-06-03 14:29:01 -0700

    removed unused code as we are instead packaging via phar

--- bootstrap.php
+++ bootstrap.php
@@ -1,16 +1,2 @@
 <?php
-ini_set('allow_url_include', 1);
-ini_set('allow_url_fopen', 1);
-
 require_once 'phar://app.phar/vendor/autoload.php';
-
-/**
- *
- */
-function checkPHPIni()
-{
-    if (! ini_get('allow_url_include') || ! ini_get('allow_url_fopen')) {
-        var_dump(ini_get('allow_url_include'), ini_get('allow_url_fopen'));
-        throw new RuntimeException('Must enable php settings: [allow_url_include] and [allow_url_fopen]');
-    }
-}

bb8614d32dd71b23b2f75178b02e6950afa9dbeb
Jake Toolson <jaketoolson@gmail.com>
2016-06-03 15:53:57 -0700

    make phar optional if using composer dependencies

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,14 @@
 <?php
-require_once 'phar://app.phar/vendor/autoload.php';
+$autoload = [
+    'phar' => 'phar://app.phar/vendor/autoload.php',
+    'vendor' => 'vendor/autoload.php'
+];
+if (file_exists($autoload['phar'])) {
+    require_once $autoload['phar'];
+} elseif (file_exists($autoload['vendor'])) {
+    require_once $autoload['vendor'];
+} else {
+    throw new RuntimeException('Dependencies missing : [app.phar, or vendor]');
+}
+
 require_once 'helpers.php';
